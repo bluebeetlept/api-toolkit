1bddc6aaf053103944ec4144eac7e3aa2f4f5baa
Bruno Gaspar <brunofgaspar1@gmail.com>
2026-09-13 17:12:09 +0100

    Bind the exception handler while registering so boot failures render (#44)

--- src/Exceptions/ConfigureExceptionHandler.php
+++ src/Exceptions/ConfigureExceptionHandler.php
@@ -11,8 +11,16 @@ use Illuminate\Foundation\Exceptions\Handler;
 
 final class ConfigureExceptionHandler
 {
+    private const string MARKER = 'api-toolkit.exceptions.configured';
+
     public function __invoke(Application $application): void
     {
+        if ($application->bound(self::MARKER)) {
+            return;
+        }
+
+        $application->instance(self::MARKER, true);
+
         $application->singleton(
             abstract: ExceptionHandlerContract::class,
             concrete: Handler::class,
